9a7a4ff5df0825835ccb2bda9dcba2e9cc4f20f7
MohamedAzarudeen <azartechnet@gmail.com>
2023-08-17 21:04:43 +0530

    17-08-2023

---
+++ LoopingP1/src/Dowhilep1.java
@@ -0,0 +1,15 @@
+
+public class Dowhilep1 {
+
+	public static void main(String[] args) {
+		
+		int i=0,n=5;
+		do
+		{
+			System.out.println("I value is::"+i);
+			i=i+1;
+		}while(i<=n);
+
+	}
+
+}
