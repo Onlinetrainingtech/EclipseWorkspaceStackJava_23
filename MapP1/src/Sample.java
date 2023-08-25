f274d032e200d926bbc9953dedf52c48c8263a61
MohamedAzarudeen <azartechnet@gmail.com>
2023-08-25 06:32:44 +0530

    25-08-2023

---
+++ MapP1/src/Sample.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Sample {
+
+	HashMap<Integer,String>list1=new HashMap<Integer, String>();
+	
+	void get1()
+	{
+	   list1.put(1001,"azar");
+	   list1.put(1002,"mohamed");
+	   list1.put(1003,"raja");
+	   
+	   for(Map.Entry h1:list1.entrySet())
+	   {
+		   System.out.println(h1.getKey());
+		   System.out.println(h1.getValue());
+		   
+	   }
+	   System.out.println("Searching the data::"+list1.containsKey(1001));
+	   System.out.println("Remove the data is::"+list1.remove(1001));
+	   System.out.println("After Removing size of the data::"+list1.size());
+	   System.out.println("Total data is::"+list1);
+	}
+	
+	public static void main(String[] args) {
+		
+		Sample s1=new Sample();
+		s1.get1();
+
+	}
+
+}
